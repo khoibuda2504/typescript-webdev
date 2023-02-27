24d9a7207655f2b8a9085af004ddf3bc4af70031
dangkhoi2504 <khoi.bd@whysoft.vn>
2023-02-27 17:11:15 +0700

    27/02

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,66 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useMemo, useState } from "react";
+import { Container } from "react-bootstrap";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { NewNote } from "./NewNote";
+import { useLocalStorage } from "./useLocalStorage";
+import { v4 as uuidV4 } from "uuid";
+export type RawNote = {
+  id: string;
+} & RawNoteData;
+
+export type RawNoteData = {
+  title: string;
+  markdown: string;
+  tagIds: string[];
+};
+export type Note = {
+  id: string;
+} & NoteData;
+
+export type Tag = {
+  id: string;
+  label: string;
+};
+
+export type NoteData = {
+  title: string;
+  markdown: string;
+  tags: Tag[];
+};
+
 function App() {
-  return <h1>Hi</h1>
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
+  const notesWithTags = useMemo(() => {
+    return notes.map((note) => {
+      return {
+        ...note,
+        tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+      };
+    });
+  }, [notes, tags]);
+  function onCreateNote({ tags, ...data }: NoteData) {
+    setNotes((prev) => {
+      return [
+        ...prev,
+        { ...data, id: uuidV4(), tagIds: tags.map((tag) => tag.id) },
+      ];
+    });
+  }
+  return (
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/new" element={<NewNote onSubmit={onCreateNote} />} />
+        <Route path=":id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  );
 }
 
-export default App
+export default App;
