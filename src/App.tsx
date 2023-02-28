cbb1c25e0fca203dced09fbdd6c4e59d6a2d68e2
dangkhoi2504 <khoi.bd@whysoft.vn>
2023-02-28 17:33:38 +0700

    28/02

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import { Container } from "react-bootstrap";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { NewNote } from "./NewNote";
 import { useLocalStorage } from "./useLocalStorage";
-import { v4 as uuidV4 } from "uuid";
+import { v4 as uuidv4 } from "uuid";
+import { NoteList } from "./NoteList";
 export type RawNote = {
   id: string;
 } & RawNoteData;
@@ -44,15 +45,27 @@ function App() {
     setNotes((prev) => {
       return [
         ...prev,
-        { ...data, id: uuidV4(), tagIds: tags.map((tag) => tag.id) },
+        { ...data, id: uuidv4(), tagIds: tags.map((tag) => tag.id) },
       ];
     });
+  }
+  function addTag(tag: Tag) {
+    setTags((prev) => [...prev, tag]);
   }
   return (
     <Container className="my-4">
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/new" element={<NewNote onSubmit={onCreateNote} />} />
+        <Route path="/" element={<NoteList availableTags={tags} notes={notesWithTags} />} />
+        <Route
+          path="/new"
+          element={
+            <NewNote
+              onSubmit={onCreateNote}
+              onAddTag={addTag}
+              availableTags={tags}
+            />
+          }
+        />
         <Route path=":id">
           <Route index element={<h1>Show</h1>} />
           <Route path="edit" element={<h1>Edit</h1>} />
--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,24 +1,29 @@
 import { Button, Form, Stack, Row, Col } from "react-bootstrap";
 import CreatableReactSelect from "react-select/creatable";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FormEvent, useRef, useState } from "react";
 import { NoteData, Tag } from "./App";
+import { v4 as uuidv4 } from "uuid";
 
 type NoteFormProps = {
   onSubmit: (data: NoteData) => void;
+  onAddTag: (tag: Tag) => void;
+  availableTags: Tag[];
 };
 
-export function NoteForm({ onSubmit }: NoteFormProps) {
+export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
   const titleRef = useRef<HTMLInputElement>(null);
   const markdownRef = useRef<HTMLTextAreaElement>(null);
   const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+  const navigate = useNavigate();
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
     onSubmit({
       title: titleRef.current!.value,
       markdown: markdownRef.current!.value,
-      tags: [],
+      tags: selectedTags,
     });
+    navigate("..");
   };
   return (
     <Form onSubmit={handleSubmit}>
@@ -34,7 +39,15 @@ export function NoteForm({ onSubmit }: NoteFormProps) {
             <Form.Group controlId="tags">
               <Form.Label>Tags</Form.Label>
               <CreatableReactSelect
+                onCreateOption={(label) => {
+                  const newTag = { id: uuidv4(), label };
+                  onAddTag(newTag);
+                  setSelectedTags((prev) => [...prev, newTag]);
+                }}
                 value={selectedTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                options={availableTags.map((tag) => {
                   return { label: tag.label, value: tag.id };
                 })}
                 onChange={(tags) => {
